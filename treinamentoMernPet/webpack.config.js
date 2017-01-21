01f1d90df221b4213ae87ba4e5d562e02875b9d1
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-01-21 09:44:37 -0200

    configurando babel, stormpath e webpack

---
+++ treinamentoMernPet/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        './src/app'
+    ],
+    devtool: 'eval',
+    output: {
+        path: __dirname,
+        filename: 'app.js',
+        publicPath: '/js',
+        pathinfo: true
+    },
+   module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['babel'],
+            include: path.join(__dirname, 'src')
+        }]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production'),
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+};
