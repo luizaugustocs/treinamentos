27337cd0d1a48bc7e2ab8db27e6b8886946eef24
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-05-05 00:36:02 -0300

    S1L14 - Iniciando o componente de busca

--- udemy/ModernReactWithReduce/src/index.js
+++ udemy/ModernReactWithReduce/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'; // React é pra gerenciar a criação dos componentes
 import ReactDOM from 'react-dom'; // ReactDOM é pra mandar pro DOM
-import Keys from '/keys'
+import Keys from '../keys'
+
+import SearchBar from './components/searchBar';
 
 const API_KEY = Keys.googleApiKey;
 
 const App = () => {
-    return <div> Hey!</div>;
+    return (<div>
+        <SearchBar/>
+    </div>);
 };
 
 // O primeiro parametro é o que renderizar, o segundo é onde vai colocar o componente renderizado
