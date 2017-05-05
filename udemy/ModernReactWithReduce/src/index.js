e4b34e547d208dd3c16c99d36b81793c0d95c516
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-05-04 23:37:16 -0300

    S1L11 - Mandando o componente para o DOM

--- udemy/ModernReactWithReduce/src/index.js
+++ udemy/ModernReactWithReduce/src/index.js
@@ -1,4 +1,9 @@
+import React from 'react'; // React é pra gerenciar a criação dos componentes
+import ReactDOM from 'react-dom'; // ReactDOM é pra mandar pro DOM
 
-const App = function () {
+const App = () => {
     return <div> Hey!</div>;
 };
+
+// O primeiro parametro é o que renderizar, o segundo é onde vai colocar o componente renderizado
+ReactDOM.render(<App />, document.querySelector('.container'));
