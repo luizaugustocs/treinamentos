9d700051a15cebe41d32403ddaae84eaa65aca56
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-05-07 00:13:04 -0300

    S2L22 - Refatorando para usar componente de classe no index

--- udemy/ModernReactWithReduce/src/index.js
+++ udemy/ModernReactWithReduce/src/index.js
@@ -1,21 +1,36 @@
-import React from 'react'; // React é pra gerenciar a criação dos componentes
+import React, {Component} from 'react'; // React é pra gerenciar a criação dos componentes
 import ReactDOM from 'react-dom'; // ReactDOM é pra mandar pro DOM
 import Keys from '../keys'
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/searchBar';
 
 const API_KEY = Keys.googleApiKey;
 
-YTSearch({key: API_KEY, term: 'dogs'}, function(response){
-    console.log(response)
-})
 
-const App = () => {
-    return (<div>
-        <SearchBar/>
-    </div>);
-};
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {videos : []};
+
+        YTSearch({key: API_KEY, term: 'dogs'}, (videos) => {
+            this.setState({ videos })
+            // this.setState({ videos: [videos]})
+            // as duas formas são iguais, mas se o nome do parametro é igual ao objeto
+            // o ES6 faz bruxaria e pode usar só o nome do objeto
+        });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
+}
 
 // O primeiro parametro é o que renderizar, o segundo é onde vai colocar o componente renderizado
 ReactDOM.render(<App />, document.querySelector('.container'));
