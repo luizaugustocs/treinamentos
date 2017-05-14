eb30a0296efa1fcb22ac073bc9c018fab73fbdf8
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-05-14 19:43:29 -0300

    S4L49 - Criando o componente de selecionar pagina e ligando ele no redux

---
+++ udemy/ModernReactWithRedux/BookShelf/src/containers/BookDetail.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux';
+
+
+class BookDetail extends Component{
+    render() {
+        return (
+            <div>
+               Book Detail.
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        activeBook: state.activeBook
+    }
+
+}
+
+export default connect(mapStateToProps)(BookDetail);
+
