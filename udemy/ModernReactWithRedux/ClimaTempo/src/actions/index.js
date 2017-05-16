54945938cb3a60f75ade61340a292da2041b2368
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-05-16 20:04:08 -0300

    S5L58 - Mandando a request para a api

--- udemy/ModernReactWithRedux/ClimaTempo/src/actions/index.js
+++ udemy/ModernReactWithRedux/ClimaTempo/src/actions/index.js
@@ -1,7 +1,7 @@
 import Keys from '../../keys'
 import axios from 'axios';
 
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${Keys.forecastKey}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${Keys.forecastKey}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
