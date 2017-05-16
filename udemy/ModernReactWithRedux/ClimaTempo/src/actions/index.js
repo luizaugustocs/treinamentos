71cce627d0c0149b49820ea8a7017bee0d27e567
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-05-15 23:01:22 -0300

    S5L57 - Montando a request

--- udemy/ModernReactWithRedux/ClimaTempo/src/actions/index.js
+++ udemy/ModernReactWithRedux/ClimaTempo/src/actions/index.js
@@ -1,3 +1,17 @@
 import Keys from '../../keys'
+import axios from 'axios';
 
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${Keys.forecastKey}`;
 
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(cityName){
+
+    const url = `${ROOT_URL}&q=${cityName},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload : request
+    }
+}
