7b404d4e832655c48afcd65edb17abb4ff3d8c5f
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-05-20 17:33:34 -0300

    S5L67 - Finalizando a integracao com o googleMaps

--- udemy/ModernReactWithRedux/ClimaTempo/src/components/GoogleMap.js
+++ udemy/ModernReactWithRedux/ClimaTempo/src/components/GoogleMap.js
@@ -6,7 +6,7 @@ class GoogleMap extends Component{
             zoom: 12,
             center: {
                 lat: this.props.lat,
-                lng: this.props.log
+                lng: this.props.lon
             }
         })
     }
--- udemy/ModernReactWithRedux/ClimaTempo/src/containers/WeatherList.js
+++ udemy/ModernReactWithRedux/ClimaTempo/src/containers/WeatherList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Chart from "../components/Chart";
+import GoogleMap from "../components/GoogleMap";
 
 class WeatherList extends Component {
 
@@ -14,9 +15,12 @@ class WeatherList extends Component {
         const temps = cityData.list.map((weather) => weather.main.temp - 273 );
         const pressures = cityData.list.map((weather) => weather.main.pressure );
         const humidities = cityData.list.map((weather) => weather.main.humidity );
+        // const lon = cityData.city.coord.lon;
+        // const lat = cityData.city.coord.lat;
+        const {lon, lat} = cityData.city.coord;
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td> <GoogleMap lat={lat} lon={lon}/></td>
                 <td> <Chart data={temps} color="orange" unit="ºC" /> </td>
                 <td> <Chart data={pressures} color="green" unit="hPa" /> </td>
                 <td> <Chart data={humidities} color="black" unit="%" /> </td>
