9ee3a140c63937318bb938ef02e520977b46e356
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-05-20 17:16:29 -0300

    S5L65 - Melhorando o componente de graficos

--- udemy/ModernReactWithRedux/ClimaTempo/src/containers/WeatherList.js
+++ udemy/ModernReactWithRedux/ClimaTempo/src/containers/WeatherList.js
@@ -11,13 +11,15 @@ class WeatherList extends Component {
 
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = cityData.list.map((weather) => weather.main.temp );
+        const temps = cityData.list.map((weather) => weather.main.temp - 273 );
+        const pressures = cityData.list.map((weather) => weather.main.pressure );
+        const humidities = cityData.list.map((weather) => weather.main.humidity );
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <td>
-                   <Chart data={temps} color="orange" />
-                </td>
+                <td> <Chart data={temps} color="orange" unit="ºC" /> </td>
+                <td> <Chart data={pressures} color="green" unit="hPa" /> </td>
+                <td> <Chart data={humidities} color="black" unit="%" /> </td>
             </tr>
         )
     }
@@ -29,9 +31,9 @@ class WeatherList extends Component {
                 <thead>
                 <tr>
                     <th>City</th>
-                    <th>Temperature</th>
-                    <th>Pressure</th>
-                    <th>Humidity</th>
+                    <th>Temperature (ºC)</th>
+                    <th>Pressure (hPa)</th>
+                    <th>Humidity (%)</th>
                 </tr>
                 </thead>
                 <tbody>
