176d022c133da5c983d1bd36e3a063bc3e3fec3b
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-05-30 23:29:01 -0300

    S9L120 - Primeiro exemplo com o router

--- udemy/ModernReactWithRedux/Blog/src/index.js
+++ udemy/ModernReactWithRedux/Blog/src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+    render() {
+        return <div>Sup Motherfucker!</div>
+    }
+}
+class Goodbye extends React.Component {
+    render() {
+        return <div>Adios Motherfucker!</div>
+    }
+}
+
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <div>
+                    Header
+                </div>
+                <Route path="/hello" component={Hello}/>
+                <Route path="/goodbye" component={Goodbye}/>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.container'));
