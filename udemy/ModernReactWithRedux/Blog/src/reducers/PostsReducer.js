bb5d9ff3348192619c32d606a6115cfbda7ac9dc
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-06-13 20:55:39 -0300

    S9L125 - Criando o reducer para armazenar os posts

---
+++ udemy/ModernReactWithRedux/Blog/src/reducers/PostsReducer.js
@@ -0,0 +1,11 @@
+import {GET_POST} from '../actions'
+import _ from 'lodash';
+
+export default function(state = {}, action){
+    switch (action.type){
+        case GET_POST:
+            return _.mapKeys(action,'id');
+        default:
+            return state;
+    }
+}
