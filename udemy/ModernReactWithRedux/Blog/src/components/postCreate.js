ff3d97ab8afda148f89b20d33e763a32e4255384
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-06-13 21:24:11 -0300

    S9L128 - Montando o componente de novo post

---
+++ udemy/ModernReactWithRedux/Blog/src/components/postCreate.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class PostNew extends Component{
+    render (){
+        return (
+            <div>
+                New Post
+            </div>
+        )
+    }
+}
+
+export default PostNew;
--- udemy/ModernReactWithRedux/Blog/src/index.js
+++ udemy/ModernReactWithRedux/Blog/src/index.js
@@ -4,17 +4,18 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Promise from 'redux-promise';
-
 import reducers from './reducers';
 import PostList from './components/postList'
-const createStoreWithMiddleware = applyMiddleware(Promise)(createStore);
+import PostCreate from './components/postCreate';
 
+const createStoreWithMiddleware = applyMiddleware(Promise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <BrowserRouter>
             <div>
                 <Route path="/" component={PostList}/>
+                <Route path="/posts/new" component={PostCreate}/>
             </div>
         </BrowserRouter>
     </Provider>
