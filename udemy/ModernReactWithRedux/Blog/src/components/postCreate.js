7a652fdcda22364bd79177e3adccf4b34571e815
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-06-13 22:52:26 -0300

    S9L137 - Setando o evento de submit do form

--- udemy/ModernReactWithRedux/Blog/src/components/postCreate.js
+++ udemy/ModernReactWithRedux/Blog/src/components/postCreate.js
@@ -11,40 +11,40 @@ class PostNew extends Component {
                     type="text"
                     {...field.input}
                 />
-                {field.meta.error}
+                <p className="text-danger">{field.meta.error}</p>
             </div>
         )
     };
 
+    onSubmit(values){
+        console.log(values);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field name="title" label="Title" component={this.renderField}/>
                 <Field name="categories" label="Categories" component={this.renderField}/>
                 <Field name="content" label="Post Content" component={this.renderField}/>
+                <button type="submit" className="btn btn-success">Save</button>
             </form>
         )
     }
 }
 
 function validate(values) {
-    console.log(values);
     const errors = {};
-
-
     if (!values.title) {
         errors.title = 'Enter a title!'
     }
-    if (!values.title) {
+    if (!values.categories) {
         errors.categories = 'Enter some categories!'
     }
-
     if (!values.content) {
         errors.content = 'Enter some content!'
     }
-
     return errors;
-
 }
 
 export default reduxForm({
