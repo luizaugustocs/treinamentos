ac7e1c83f18347250d7f8c0e5fbf33574d86ebee
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-06-18 21:47:40 -0300

    S9L145 - Chamando a action a hora que entra no componente para garantir que o post está carregado

--- udemy/ModernReactWithRedux/Blog/src/components/postShow.js
+++ udemy/ModernReactWithRedux/Blog/src/components/postShow.js
@@ -1,6 +1,16 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions'
+
 
 class PostsShow extends Component {
+
+    componentDidMount() {
+        const {id} = this.props.match.params; // this.props.match é um objeto setado pelo router
+                                              // com os matchs dos parametros da rota
+        this.props.fetchPost(id);
+    }
+
     render() {
         return (
             <div>
@@ -8,7 +18,10 @@ class PostsShow extends Component {
             </div>
         )
     }
-
 }
 
-export default PostsShow;
+function mapStateToProps(state, ownProps) { // o segundo parametro são os parametros do componente
+    return {post: state.posts[ownProps.match.params.id]};
+}
+
+export default connect(mapStateToProps, {fetchPost})(PostsShow);
