175780281187ab7655712464a7020bee113f899f
Luiz Augusto <luizaugustocsouza@gmail.com>
2017-08-15 21:21:14 -0300

    Comecando o tutorial de graphql

---
+++ graphql-node/src/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
+
+import schema from './schema';
+
+let app = express();
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+
+
+app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql'
+}));
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`GraphQL server listening on port ${PORT}`);
+});
