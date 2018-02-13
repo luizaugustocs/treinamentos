63a5e42421f80cfff99bf0fc181840e79b95d8ed
Luiz Augusto <luizaugustocsouza@gmail.com>
2018-02-13 10:29:11 -0200

    S7L30 - Criando o albumList

--- udemy/CompleteReactNativeWithRedux/albums/index.js
+++ udemy/CompleteReactNativeWithRedux/albums/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 // import App from './App';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-  <Header text="Albums" />
+    <View>
+        <Header text="Albums" />
+        <AlbumList />
+    </View>
 );
 AppRegistry.registerComponent('albums', () => App);
