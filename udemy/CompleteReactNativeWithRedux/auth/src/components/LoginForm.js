a53ad6b93305a1e273ae12105987c0dc41b91220
Luiz Augusto C. Souza <luizaugustocsouza@gmail.com>
2018-03-28 21:09:52 -0300

    S9L66 - Estilizando o componente de input

--- udemy/CompleteReactNativeWithRedux/auth/src/components/LoginForm.js
+++ udemy/CompleteReactNativeWithRedux/auth/src/components/LoginForm.js
@@ -10,7 +10,7 @@ class LoginForm extends React.Component {
             <Card>
                 <CardSection>
                     <Input
-                        label=""
+                        label="Email"
                         onChangeText={text => this.setState({ text })}
                         value={this.state.text}
                     />
