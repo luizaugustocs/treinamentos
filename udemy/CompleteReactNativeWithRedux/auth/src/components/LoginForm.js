700bf00edef2c27e140c3adf6c65e2a5deb87c83
Luiz Augusto C. Souza <luizaugustocsouza@gmail.com>
2018-03-28 21:33:28 -0300

    S10L70 - Fluxo básico de autenticaçāo

--- udemy/CompleteReactNativeWithRedux/auth/src/components/LoginForm.js
+++ udemy/CompleteReactNativeWithRedux/auth/src/components/LoginForm.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import firebase from 'firebase';
+import { Text } from 'react-native';
 import { Card, CardSection, Button, Input } from './common';
 
 class LoginForm extends React.Component {
     state = {
         email: '',
-        password: ''
+        password: '',
+        error: ''
     };
 
     onLogin = () => {
-        const {email, password} = this.state;
-        firebase.auth().signInWithEmailAndPassword(email, password);
+        const { email, password } = this.state;
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({ error: 'Authentication Failed' });
+                    });
+            });
     }
 
     render() {
@@ -33,6 +41,7 @@ class LoginForm extends React.Component {
                         value={this.state.password}
                     />
                 </CardSection>
+                <Text style={styles.errorTextStyle}>{this.state.error}</Text>
                 <CardSection>
                     <Button onPress={this.onLogin}>
                         Log In
@@ -41,6 +50,14 @@ class LoginForm extends React.Component {
             </Card>
 
         );
+    }
+}
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
     }
 }
 
