7faa9e9c9e660dddc37580a52d829894868b762b
Luiz Augusto C. Souza <luizaugustocsouza@gmail.com>
2018-03-30 16:27:17 -0300

    S12L88 - Chamando o connect do react-redux

---
+++ udemy/CompleteReactNativeWithRedux/tech_stack/src/components/LibraryList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class LibraryList extends React.Component {
+
+    render() {
+        return;
+    }
+}
+
+
+export default connect()(LibraryList);
