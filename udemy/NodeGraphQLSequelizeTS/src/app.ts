b1d8c1abb72b2d1ff92ae16024437b3283e1cb70
Luiz Augusto C. Souza <luizaugustocsouza@gmail.com>
2018-07-15 21:52:25 -0300

    S3L22 - Habilitando o graphiql

--- udemy/NodeGraphQLSequelizeTS/src/app.ts
+++ udemy/NodeGraphQLSequelizeTS/src/app.ts
@@ -13,7 +13,8 @@ class App {
 
     private middleware(): void {
         this.express.use('/graphql', expressGraphQL({
-            schema
+            schema,
+            graphiql: process.env.NODE_ENV === 'development'
         }))
     }
 }
