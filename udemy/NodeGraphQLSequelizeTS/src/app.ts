60bead84d0e0cc019e410efcc15356277e41172c
Luiz Augusto C. Souza <luizaugustocsouza@gmail.com>
2018-07-21 16:00:44 -0300

    S6L52 - Adicionando a conexao do banco no context do graphql

--- udemy/NodeGraphQLSequelizeTS/src/app.ts
+++ udemy/NodeGraphQLSequelizeTS/src/app.ts
@@ -1,21 +1,24 @@
 import * as express from 'express';
 import * as expressGraphQL from 'express-graphql';
 import schema from './graphql/schema';
+import db from './models';
 
 class App {
 
     public express: express.Application;
 
-    constructor(){
+    constructor() {
         this.express = express();
         this.middleware();
     }
 
     private middleware(): void {
-        this.express.use('/graphql', expressGraphQL({
-            schema,
-            graphiql: process.env.NODE_ENV === 'development'
-        }))
+        this.express.use('/graphql',
+            expressGraphQL({
+                schema,
+                graphiql: process.env.NODE_ENV === 'development',
+                context: db
+            }))
     }
 }
 
