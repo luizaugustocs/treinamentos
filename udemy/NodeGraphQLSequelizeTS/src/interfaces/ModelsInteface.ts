f40f9e34d8fa4fd549707647c1717f2adcf38b62
Luiz Augusto C. Souza <luizaugustocsouza@gmail.com>
2018-07-21 14:46:08 -0300

    S4L38 - Montando o CommentModel

--- udemy/NodeGraphQLSequelizeTS/src/interfaces/ModelsInteface.ts
+++ udemy/NodeGraphQLSequelizeTS/src/interfaces/ModelsInteface.ts
@@ -1,7 +1,9 @@
 import {UserModel} from "../models/UserModel";
 import {PostModel} from "../models/PostModel";
+import {CommentModel} from "../models/CommentModel";
 
 export interface ModelsInteface {
     User: UserModel,
     Post: PostModel,
+    Comment: CommentModel,
 }
