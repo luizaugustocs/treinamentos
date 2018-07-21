e921851d6856f19d86f131d61aa12399b4ce8722
Luiz Augusto C. Souza <luizaugustocsouza@gmail.com>
2018-07-21 15:32:54 -0300

    S5L45 - Modularizando os tipos

--- udemy/NodeGraphQLSequelizeTS/src/graphql/schema.ts
+++ udemy/NodeGraphQLSequelizeTS/src/graphql/schema.ts
@@ -1,49 +1,15 @@
 import {makeExecutableSchema} from 'graphql-tools';
+import {Query} from "./query";
+import {Mutation} from "./mutation";
 
-const users: any[] = [
-    {
-        id: 1,
-        name: 'Jhon',
-        email: 'jhon@email.com'
-    },
-    {
-        id: 2,
-        name: 'Dany',
-        email: 'dany@email.com'
+const SchemaDefinition = `
+    type Schema {
+        query: Query
+        mutation: Mutation
     }
-];
-
-const typeDefs = `
-    type User {
-        id: ID!
-        name: String!
-        email: String!
-    }
-    
-    type Query {
-        allUsers: [User!]!
-    }
-    
-    type Mutation {
-        createUser(name: String!, email: String!): User!
-    }
-
 `;
 
-const resolvers = {
-    Query: {
-        allUsers: () => users
-    },
-    Mutation: {
-        createUser: (parent, params) => {
-            const newUser = {
-                id: users.length + 1,
-                ...params
-            };
-            users.push(newUser);
-            return newUser;
-        }
-    }
-};
-
-export default makeExecutableSchema({typeDefs, resolvers});
+export default makeExecutableSchema({typeDefs: [
+        SchemaDefinition, Query, Mutation
+    ]
+});
