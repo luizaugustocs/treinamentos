1b2f3e3c1796774c4efe3f102a699488e7650654
Luiz Augusto C. Souza <luizaugustocsouza@gmail.com>
2019-04-03 11:15:46 -0300

    S8L94 - Criando factory para os dataloaders

---
+++ udemy/NodeGraphQLSequelizeTS/src/graphql/dataloaders/DataLoaderFactory.ts
@@ -0,0 +1,21 @@
+import {DBConnection} from "../../interfaces/DBConnectionInterface";
+import {DataLoaders} from "../../interfaces/DataLoadersInterface";
+import * as DataLoader from "dataloader";
+import {UserInstance} from "../../models/UserModel";
+import {UserLoader} from "./UserLoader";
+import {PostInstance} from "../../models/PostModel";
+import {PostLoader} from "./PostLoader";
+
+export class DataLoaderFactory {
+
+    constructor(private db: DBConnection) {
+
+    }
+
+    getLoaders(): DataLoaders {
+        return {
+            userLoader: new DataLoader<number, UserInstance>((ids: number[]) => UserLoader.batchUsers(this.db.User, ids)),
+            postLoader: new DataLoader<number, PostInstance>((ids: number[]) => PostLoader.batchPosts(this.db.Post, ids)),
+        }
+    }
+}
--- udemy/NodeGraphQLSequelizeTS/src/graphql/dataloaders/PostLoader.ts
+++ udemy/NodeGraphQLSequelizeTS/src/graphql/dataloaders/PostLoader.ts
@@ -1,7 +1,7 @@
 import {PostInstance, PostModel} from "../../models/PostModel";
 
 export class PostLoader {
-    static batchUsers(post: PostModel, ids: number[]): Promise<PostInstance[]> {
+    static batchPosts(post: PostModel, ids: number[]): Promise<PostInstance[]> {
         return Promise.resolve(post.findAll({
             where: {id: {$in: ids}}
         }).value())
